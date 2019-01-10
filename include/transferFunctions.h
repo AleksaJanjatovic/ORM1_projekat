17b51f00f64a3a87e3cb378984872fd077110d01
AleksaJanjatovic <43210764+AleksaJanjatovic@users.noreply.github.com>
2019-01-10 23:23:30 +0100

    Delete transferFunctions.h

--- include/transferFunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSFER_FUNCTION
-#define TRANSFER_FUNCTION
-#include "include/protocolStructure.h"
-
-int sendData(char*, unsigned int, RPAddr);
-int recvData(char*, unsigned int, RPAddr);
-
-#endif
