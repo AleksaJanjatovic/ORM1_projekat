c77e730492a431acd90f2dc9064b661de0c96a3a
AleksaJanjatovic <43210764+AleksaJanjatovic@users.noreply.github.com>
2019-01-14 18:00:16 +0100

    Add files via upload

--- routerProgram.c
+++ routerProgram.c
@@ -4,68 +4,144 @@
 #include <string.h>
 #include <pthread.h>
 
-int main(int argc, char * argv[]) {
-    routerModel rm;
-    if(argc < 6) {
-        //printf("Not enough arguments passed");
-        return -1;
+void routerInit(routerModel * baseRouterModel) {
+    char ip_address[100];
+    unsigned short port;
+    RPAddress routerRPAddress;
+    while(1) {
+        printf("Enter the router IP address:\n");
+        scanf("%s", ip_address);
+        printf("Enter the router PORT number:\n");
+        scanf("%hu", &port);
+        getchar();
+        printf("Enter the router RPAddress:\n");
+        scanf("%s", routerRPAddress);
+        getchar();
+        if(initRM(port, ip_address, routerRPAddress, baseRouterModel) == -1) {
+            printf("ERROR! Invalid router IP address passed!\n");
+            continue;
+        }
+        break;
     }
-    routerModel destRouter;
-    RPAddress dummy;
+    printRouterModel(baseRouterModel);
+}
 
-    initRM(atoi(argv[1]), argv[2], argv[3], &rm, 0);
-    initRM(atoi(argv[4]), argv[5], dummy, &destRouter, 1);
-    connectRMToNetwork(&rm, &destRouter);
-    pthread_t dataParsing, routerTableControl;
-    ////printf("Pravljenje threada\n");
-//    //printf("Greksa: %d\n", parseTP(&rm));
-    pthread_create(&dataParsing, NULL, (void*)parseReceivedData, &rm);
-    //pthread_join(dataParsing, NULL);
-   // //printf("Pravljenje thread-a je probelm\n");
-   while(1) {
-        routerTableTimeControl(&rm);
-//        routerTableTimeControl(&rm);
-//        //printRouterTable(&rm);
-//        //printRouterModel(&rm);
-//        printRouterTable(&rm);
-         usleep(20000);
-   }
-
-    ////printRouterModel(&rm);
-
-//    char finished = 0;
-//    rm.routerTable[1][0] = 2;
-//    rm.routerTable[1][1] = 2;
-//    rm.routerTable[1][2] = 3;
-//    rm.routerTable[2][0] = 3;
-//    rm.routerTable[2][1] = 1;
-//    rm.routerTable[2][2] = 4;
-//    rm.routerTable[2][3] = 7;
-//    rm.routerTable[3][0] = 1;
-//    rm.routerTable[3][1] = 1;
-//    rm.routerTable[4][0] = 3;
-//    rm.routerTable[4][1] = 2;
-//    rm.routerTable[4][2] = 5;
-//    rm.routerTable[4][3] = 8;
-//    rm.routerTable[5][0] = 2;
-//    rm.routerTable[5][1] = 9;
-//    rm.routerTable[5][2] = 4;
-//    rm.routerTable[8][0] = 2;ls
-//    rm.routerTable[8][1] = 4;
-//    rm.routerTable[8][2] = 10;
-//    rm.routerTable[9][0] = 2;
-//    rm.routerTable[9][1] = 8;
-//    rm.routerTable[9][2] = 5;
-//    rm.routerTable[10][0] = 2;
-//    rm.routerTable[10][1] = 8;
-//    rm.routerTable[10][2] = 11;
-//    rm.routerTable[11][0] = 1;
-//    rm.routerTable[11][1] = 10;
-//    path[0] = 9;
-//    findPath(9, 7, &rm, path, &nodeNum, &finished);
-//    for(int i = 0; i <= nodeNum; i++) {
-//        //printf("Path member %d is router %d\n", i,path[i]);
+//void menu(routerModel * baseRouterModel, pthread_t * rTTcontrol, pthread_t * tpParsing) {
+//    int c = 0;
+//    char ip_address[100];
+//    unsigned short port = 0;
+//    routerModel destRouter;
+//    while(1) {
+//        //system("clear");
+//        switch (c) {
+//        case 0:
+//            printf("Main menu:\n");
+//            printf("1. Reinitialise base router\n");
+//            printf("2. Connect router to another router from network\n");
+//            printf("3. Print router model\n");
+//            printf("4. Print router table\n");
+//            scanf("%d", &c);
+//            continue;
+//            break;
+//        case 1:
+//            pthread_destroy(*rTTcontrol, 0);
+//            pthread_destroy(*tpParsing, 0);
+//            closeRM(baseRouterModel);
+//            routerInit(baseRouterModel, rTTcontrol, tpParsing);
+//            break;
+//        case 2:
+//            printf("Enter the destination router IP address:\n");
+//            scanf("%s", ip_address);
+//            printf("Enter the destination router PORT number:\n");
+//            scanf("%hu", &port);
+//            if(initRM(port, ip_address, NULL, &destRouter) == -1) {
+//                printf("ERROR! Invalid router IP address passed!\n");
+//                continue;
+//            }
+//            printf("Zivot\n");
+//            if(connectRMToNetwork(baseRouterModel, &destRouter) == -8) {
+//                printf("ERROR! Router with this RPAddress: %s is already connected to the router\n", baseRouterModel->routerAddress);
+//                c = 0;
+//            }
+//            break;
+//        case 3:
+//            printRouterModel(baseRouterModel);
+//            break;
+//        case 4:
+//            printRouterTable(baseRouterModel);
+//            break;
+//        default:
+//            printf("Wrong menu option!\n");
+//            break;
+//        }
+//        c = 0;
+//        usleep(5000000);
+//        printf("%d\n",c);
 //    }
+//}
+
+int main(int argc, char * argv[]) {
+    routerModel thisRouter;
+    routerModel destRouter;
+
+    pthread_t rTTControl, tpParsing;
+    int c = 0;
+    char ip_address[100];
+    unsigned short port = 0;
+
+    routerInit(&thisRouter);
+
+    pthread_create(&rTTControl, 0, (void*)routerTableTimeControl,  &thisRouter);
+    pthread_create(&tpParsing, 0, (void*)parseReceivedData, &thisRouter);
+
+    while(1) {
+        //system("clear");
+        switch (c) {
+        case 0:
+            printf("Main menu:\n");
+            printf("1. Reinitialise base router\n");
+            printf("2. Connect router to another router from network\n");
+            printf("3. Print router model\n");
+            printf("4. Print router table\n");
+            scanf("%d", &c);
+            continue;
+            break;
+        case 1:
+            pthread_kill(rTTControl, 0);
+            pthread_kill(tpParsing, 0);
+            closeRM(&thisRouter);
+            routerInit(&thisRouter);
+//            pthread_create(&rTTControl, 0, (void*)routerTableTimeControl,  &thisRouter);
+//            pthread_create(&tpParsing, 0, (void*)parseReceivedData, &thisRouter);
+            break;
+        case 2:
+            printf("Enter the destination router IP address:\n");
+            scanf("%s", ip_address);
+            printf("Enter the destination router PORT number:\n");
+            scanf("%hu", &port);
+            if(initRM(port, ip_address, NULL, &destRouter) == -1) {
+                printf("ERROR! Invalid router IP address passed!\n");
+                continue;
+            }
+            //printf("Zivot\n");
+            if(connectRMToNetwork(&thisRouter, &destRouter) == -8) {
+                printf("ERROR! Router with this RPAddress: %s is already connected to the router\n", thisRouter.routerAddress);
+            }
+            break;
+        case 3:
+            printRouterModel(&thisRouter);
+            break;
+        case 4:
+            printRouterTable(&thisRouter);
+            break;
+        default:
+            printf("Wrong menu option!\n");
+            break;
+        }
+        c = 0;
+        //printf("%d\n",c);
+    }
+
 
     return 0;
 }
--- userProgram.c
+++ userProgram.c
@@ -3,46 +3,149 @@
 #include "include/userHost.h"
 #include <string.h>
 #include <pthread.h>
-int main(int argc, char * argv[]) {
-    routerModel rm;
-    if(argc < 5) {
-        //printf("Not enough arguments passed");
-        return -1;
+
+typedef struct receiveStruct_t {
+    transferPackage * tp;
+    userModel * um;
+}receiveStruct;
+
+void threadReceiveTPFromRouter(receiveStruct * rs) {
+    receiveTPfromRouter(rs->tp, rs->um);
+}
+
+void initUser(userModel * baseUserModel) {
+    char ip_address[100];
+    unsigned short port;
+    while(1) {
+        printf("Enter the user IP address:\n");
+        scanf("%s", ip_address);
+        printf("Enter the user PORT number:\n");
+        scanf("%hu", &port);
+        getchar();
+        if(initUM(port, ip_address, baseUserModel) == -7) {
+            printf("ERROR! Invalid user IP address passed!\n");
+            continue;
+        }
+        break;
     }
-    userModel um;
+    printUserModel(baseUserModel);
+}
+
+int main(int argc, char * argv[]) {
+    int c;
+    userModel thisUser;
+    routerModel destRouter;
     transferPackage tp;
-//    setRouterNumber(200, tp.destinationAddress);
-//    setUserNumber(123, tp.destinationAddress);
-//    setUserNumber(202, tp.destinationAddress);
+    unsigned short port;
+    char ip_address[100];
+    memset(&tp, 0, sizeof(transferPackage));
 
-//    setRouterNumber(12, tp.sourceAddress);
-//    setUserNumber(125, tp.sourceAddress);
-    tp.packageType = 1;
-//    tp.dataSent = 10;
+    receiveStruct rs;
+    rs.tp = &tp;
+    rs.um = &thisUser;
+
+    pthread_t receiveDataThread;
+
+    tp.dataSent = 3;
+    tp.data[0] = 'A';
+    tp.data[1] = 'B';
+    tp.data[2] = 0;
+    strcpy(tp.destinationAddress, "234.002");
+    strcpy(tp.sourceAddress, "241.222");
+    tp.path[0] = 123;
+    tp.path[1] = 125;
+    tp.path[2] = 220;
+    tp.nodeNum = 23;
+    convertTPackageToArray(&tp, destRouter.sendTPBuffer);
+    convertArrayToTPackage(destRouter.sendTPBuffer, &tp);
+    printTPPackage(&tp);
+
+    initUser(&thisUser);
+
+    while(1) {
+        //system("clear");
+        switch (c) {
+        case 0:
+            printf("Main menu:\n");
+            printf("1. Reinitialise user\n");
+            printf("2. Connect user to a router from the network\n");
+            printf("3. Print user model\n");
+            printf("4. Send data\n");
+            scanf("%d", &c);
+            continue;
+            break;
+        case 1:
+            pthread_kill(&receiveDataThread, 0);
+            closeUM(&thisUser);
+            initUser(&thisUser);
+            //pthread_create
+            break;
+        case 2:
+            printf("Enter the destination router IP address:\n");
+            scanf("%s", ip_address);
+            printf("Enter the destination router PORT number:\n");
+            scanf("%hu", &port);
+            if(initRM(port, ip_address, NULL, &destRouter) == -1) {
+                printf("ERROR! Invalid router IP address passed!\n");
+                continue;
+            }
+            printf("Zivot\n");
+            connectUMToNetwork(&thisUser, &destRouter);
+            pthread_create(&receiveDataThread, 0, (void*)threadReceiveTPFromRouter, &rs);
+            break;
+        case 3:
+            printUserModel(&thisUser);
+            break;
+        case 4:
+            printf("Enter your data ( NO MORE THEN %d ):\n", CONVBUFFSIZETP);
+            scanf("%s", tp.data);
+            getchar();
+            printf("Enter the destination address\n");
+            scanf("%s", tp.destinationAddress);
+            getchar();
+            //fgets
+            //printf("Destination address is %s\n", tp.destinationAddress);
+            strcpy(tp.sourceAddress, thisUser.userAddress);
+            tp.dataSent = strlen(tp.data) + 1;
+            sendTPtoRouter(&tp, &thisUser);
+            break;
+        default:
+            printf("Wrong menu option!\n");
+            break;
+        }
+        c = 0;
+        //printf("%d\n",c);
+    }
+//    routerModel rm;
+//    if(argc < 6) {
+//        //printf("Not enough arguments passed");
+//        return -1;
+//    }
+//    userModel um;
+//    transferPackage tp;
+
+//    initUM(atoi(argv[1]), argv[2], &um);
+//    initRM(atoi(argv[3]), argv[4], NULL, &rm);
+////    //printRouterModel(&rm1);
+//    connectUMToNetwork(&um, &rm);
+//    printUserModel(&um);
+//    strcpy(tp.data, "ZABA");
+//    tp.packageType = 0;
+//    strcpy(tp.sourceAddress, um.userAddress);
+//    strcpy(tp.destinationAddress, argv[5]);
+
+
+//    //printf("sending tp:\n");
+//    tp.dataSent = 5;
+//    sendTPtoRouter(&tp, &um);
 //    //printTPPackage(&tp);
-//    convertTPackageToArray(&tp, um.transferBuffer);
-//    convertArrayToTPackage(um.transferBuffer, &tp);
+//    while(1) {
+//        receiveTPfromRouter(&tp, &um);
+//        printTPPackage(&tp);
+//        usleep(1000000);
+//    }
 //    //printTPPackage(&tp);
-
-    initUM(atoi(argv[1]), argv[2], &um);
-    RPAddress dummy;
-    initRM(atoi(argv[3]), argv[4], dummy, &rm, 1);
-//    //printRouterModel(&rm1);
-//    //printUserModel(&um);
-    connectUMToNetwork(&um, &rm);
-    strcpy(tp.data, "ZABA");
-    tp.packageType = 0;
-    strcpy(tp.sourceAddress, um.userAddress);
-    strcpy(tp.destinationAddress, "012.002");
-
-    sendTPtoRouter(&tp, &um, 5);
-    //printf("sending tp:\n");
-    tp.dataSent = 5;
-    //printTPPackage(&tp);
-    receiveTPfromRouter(&tp, &um);
-    //printTPPackage(&tp);
-    while(1);
-    closeRM(&rm);
-    closeUM(&um);
+//    closeRM(&rm);
+//    closeUM(&um);
     return 0;
 }
